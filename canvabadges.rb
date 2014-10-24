f30458612220949e12d78da6f22b474210d8e1ff
Kulgar <kulgar@ct2c.fr>
2014-10-24 13:15:52 +0200

    Added i18n Gem and tested locales

--- canvabadges.rb
+++ canvabadges.rb
@@ -1,4 +1,6 @@
 require 'sinatra/base'
+require 'i18n'
+require 'i18n/backend/fallbacks'
 require 'oauth'
 require 'json'
 require 'dm-core'
@@ -23,10 +25,10 @@ class Canvabadges < Sinatra::Base
   register Sinatra::Api
   register Sinatra::BadgeConfiguration
   register Sinatra::Views
-  
+
   use Rack::Iframe
   use DomainFudger
-  
+
   # sinatra wants to set x-frame-options by default, disable it
   disable :protection
   # enable sessions so we can remember the launch info between http requests, as
@@ -34,14 +36,22 @@ class Canvabadges < Sinatra::Base
   enable :sessions
   raise "session key required" if ENV['RACK_ENV'] == 'production' && !ENV['SESSION_KEY']
   set :session_secret, ENV['SESSION_KEY'] || "local_secret"
+  set :root, File.dirname(__FILE__)
 
   env = ENV['RACK_ENV'] || settings.environment
+  puts env
   DataMapper.setup(:default, (ENV["DATABASE_URL"] || "sqlite3:///#{Dir.pwd}/#{env}.sqlite3"))
   DataMapper.auto_upgrade!
-  
+
   configure :production do
     require 'rack-ssl-enforcer'
     use Rack::SslEnforcer
+  end
+
+  configure do
+    I18n::Backend::Simple.send(:include, I18n::Backend::Fallbacks)
+    I18n.load_path = Dir[File.join(settings.root, 'locales', '*.yml')]
+    I18n.backend.load_translations
   end
 end
 
