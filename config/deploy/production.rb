790962a089311f2aa07ce00356a7e4ba12c6050e
Kulgar <kulgar@ct2c.fr>
2015-12-18 22:36:15 +0100

    Updated deploy server config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,8 +6,9 @@
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
-server '46.105.116.33', user: 'deploy', roles: %w{app db web}
-#server '5.135.140.62', user: 'deploy', roles: %w{app db web}
+#server '46.105.116.33', user: 'deploy', roles: %w{app db web}
+server '5.135.140.62', user: 'deploy', roles: %w{app db web}, port: 33565
+#server "5.39.74.49", user: "deploy", roles: %w{web app db}, port: 33565
 
 # role-based syntax
 # ==================
